cffff4e789cc6bf29f2e81e86af94e78d07776b6
Ziwei Yang <85477529+yangziwei96@users.noreply.github.com>
2025-03-03 13:53:26 +0900

    Add files via upload

--- Calculate_Initial_Remaining_Gene_Set.py
+++ Calculate_Initial_Remaining_Gene_Set.py
@@ -2,15 +2,26 @@ import os
 import pandas as pd
 
 def read_csv_files(folder_path):
-    # Get a list of all CSV files in the folder
-    csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+    # Define the subdirectory
+    subfolder_path = os.path.join(folder_path, "Example_gene_deletion_data")
     
+    # Ensure the directory exists
+    if not os.path.exists(subfolder_path):
+        print(f"Warning: The folder '{subfolder_path}' does not exist.")
+        return []
+
+    # Get a list of all CSV files in the subfolder
+    csv_files = [f for f in os.listdir(subfolder_path) if f.endswith('.csv')]
+    
+    # Print the number of CSV files found
+    print(f"Found {len(csv_files)} CSV files in '{subfolder_path}'.")
+
     # Initialize a list to hold sets of data from each CSV file
     data_sets = []
     
     for csv_file in csv_files:
         # Read the CSV file into a DataFrame
-        df = pd.read_csv(os.path.join(folder_path, csv_file))
+        df = pd.read_csv(os.path.join(subfolder_path, csv_file))
         
         # Ensure there's only one column and get the data as a set (excluding the title)
         if len(df.columns) == 1:
@@ -25,8 +36,10 @@ def calculate_intersection(data_sets):
     # Calculate the intersection of all sets
     if data_sets:
         intersection_set = set.intersection(*data_sets)
+        print(f"Initial remaining gene set: {len(intersection_set)} genes.")
         return intersection_set
     else:
+        print("Initial remaining gene set: 0 gene.")
         return set()
 
 def save_to_csv(data_set, output_file):
@@ -45,16 +58,16 @@ def main(folder_path, output_file):
     
     # Save the result to a new CSV file
     save_to_csv(common_data, output_file)
-    print(f"The intersection of the remaining gene sets has been saved to {output_file}")
+    print(f"The initial remaining gene set has been saved to {output_file}")
 
 if __name__ == "__main__":
     # Get the path of the script
     script_dir = os.path.dirname(os.path.abspath(__file__))
     
-    # Set default folder path to the script's directory
+    # Set folder path to the script's directory
     folder_path = script_dir
     
-    # Set default output file path within the script's directory
+    # Set output file path within the script's directory
     output_file = os.path.join(script_dir, "Remaining_gene.csv")
 
     # # Change to your folder path
